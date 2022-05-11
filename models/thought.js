763a3a3f3519523d6d9c81002fd9b7ff7b018315
Paul Shertzinger <pshertzi@gmail.com>
2022-05-11 16:49:34 -0400

    basic routes

--- models/thought.js
+++ models/thought.js
@@ -1,1 +1,18 @@
-const {schema, model } = require('mongoose');
+const {model, Schema } = require('mongoose');
+
+const ThoughtSchema = new Schema({
+    writtenBy: {
+        type: String
+    },
+    thoughtBody: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
